1afd2b5fb858ded61c7526748c47430b33347919
miganaw <misge1298@gmail.com>
2020-12-13 22:18:58 +0300

    delivery model changed

--- models/delivery.model.js
+++ models/delivery.model.js
@@ -1,25 +1,32 @@
-const { Schema, model } = require("mongoose");
-const Location  = require("./location.model");
+const { Schema, model } = require('mongoose');
+const Location = require('./location.model');
 
-const deliverySchema = new Schema({
-  
-  deliveryId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-
+const deliverySchema = new Schema(
+  {
+    deliveryId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    pickup_Location: {
+      type: Location,
+    },
+    dropoff_Location: {
+      type: Location,
+    },
+    quintal_per_km: {
+      type: Number,
+      required: true,
+    },
+    rate: {
+      type: Number,
+      default: 1,
+      enum: [1, 2, 3, 4, 5],
+    },
+    comment: {
+      type: String,
+    },
   },
+  { timestamps: true }
+);
 
-  pickup_location: {
-    type: Location
-  },
-  dropoff_location: {
-    type: Location
-
-  },
-  
-
-
-
-}, { timestamps: true });
-
-module.exports = model("Delivery", deliverySchema);
+module.exports = model('Delivery', deliverySchema);
